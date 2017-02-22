2326ff29ba48e710d57af6d61e1917dada3ec930
Dzmitry Penda <dzmitry.penda@itechart-group.com>
2017-02-22 11:11:42 +0300

    Fixes to start the app on mobile.

--- app/config/reducers.js
+++ app/config/reducers.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import authReducer from '../common/reducers/authReducer';
+import authReducer from '../common/reducers/auth.reducer';
 import dashboardReducer from '../common/reducers/dashboard.reducer';
 import profileReducer from '../common/reducers/profile.reducer';
 import projectReducer from '../common/reducers/project.reducer';
