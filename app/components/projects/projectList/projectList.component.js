dffac3b991fb9dbba5def96206188782d8750929
Dmitry Penda <dzmitry.penda@itechart-group.com>
2017-04-30 20:07:34 +0300

    Added project list view.

--- app/components/projects/projectList/projectList.component.js
+++ app/components/projects/projectList/projectList.component.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { AppRegistry, StyleSheet, Text, ScrollView, View, TextInput, TouchableOpacity } from 'react-native';
 import Button from 'react-native-button';
 
+import ProjectCard from './projectCard/projectCard.component'
+
 export default class ProjectListComponent extends Component {
+    static navigationOptions = ({ navigation, screenProps }) => ({
+        title: 'Ваши проекты',
+        headerRight: <Button containerStyle={styles.addProjectButton} style={styles.addProjectButtonContent}
+                        onPress={()=>{navigation.navigate('CreateProject', {isEdit: false})}}> + </Button>
+    });
+
     constructor(props){
         super(props)
         
@@ -15,33 +23,51 @@ export default class ProjectListComponent extends Component {
     }
 
     createProject() {
-        this.props.navigation.navigate('CreateProject')
+        this.props.navigation.navigate('CreateProject', {isEdit: false})
     }
 
     render() {
         console.log('*******************************************************************')
         console.log('RENDER: PROJECT LIST *************************************************')
         console.log('*******************************************************************')
-        const { projects } = this.props.projects; 
+        const { projects }  = this.props.projects;
+
         if(!projects.length){
             return (
                 <View style={styles.container}>
                     <Text style={styles.welcome}>
-                        There are no projects for now.You can
+                        Здесь пока пусто. Вы можете
                     </Text> 
                     <TouchableOpacity onPress={(this.createProject.bind(this))}>
-                        <Text>create one</Text>
+                        <Text>создать новый проект</Text>
                     </TouchableOpacity>
                 </View>
             );
+        } else {
+            const activeProjects  = projects.filter((project) => project.current);
+            const inactiveProjects  = projects.filter((project) => !project.current);
+
+            return (
+                <View style={styles.container}>
+                    { !!activeProjects.length &&
+                        <ScrollView>
+                            <Text style={styles.projectSectionHeader}>
+                                Активные проекты
+                            </Text> 
+                            {activeProjects.map((project, index) => <ProjectCard key={index} project={project}></ProjectCard>)}
+                        </ScrollView>
+                    }
+                    { !!inactiveProjects.length &&
+                        <ScrollView>
+                            <Text style={styles.projectSectionHeader}>
+                                Завершенные проекты
+                            </Text>
+                            {inactiveProjects.map((project, index) => <ProjectCard key={index} project={project}></ProjectCard>)}
+                        </ScrollView>
+                    }
+                </View>
+            )
         }
-        return (
-            <View style={styles.container}>
-                <Text style={styles.welcome}>
-                    Projects:
-                </Text>
-            </View>
-        )
     }
 }
 
@@ -60,6 +86,12 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         margin: 10,
     },
+    projectSectionHeader: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 20,
+        marginLeft: 20,
+    },
     instructions: {
         textAlign: 'center',
         color: '#333333',
@@ -67,7 +99,7 @@ const styles = StyleSheet.create({
     }, 
     register: {
         textAlign: 'center',
-        color: '#333333',
+        color: '#333333', 
         marginBottom: 5,
         fontWeight: 'bold',
         marginTop: 30
@@ -78,7 +110,18 @@ const styles = StyleSheet.create({
         marginRight: 20
     },
     submitButton: {
-        color:"#fff",
+        color:"#FFF",
+    },
+    addProjectButton:{
+        backgroundColor: 'green',
+        height: 30,
+        width: 30,
+        borderRadius: 15,
+        marginRight: 10
+    },
+    addProjectButtonContent:{
+        color: '#FFF',
+        fontSize: 20,
     }
 });
 
